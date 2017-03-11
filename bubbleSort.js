cdfe1ef15674f4fba83c2de503ab23299f1060d9
Nick Kasten <kastencode@gmail.com>
2017-03-10 18:32:01 -0600

    bubble sort added

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,2 +1,18 @@
 // Bubble Sort Example
-var myArray = 
+
+var argNums = process.argv.slice(2).map(function(str) {return Number(str)})
+var swapped
+
+do {
+  swapped = false
+  for (var i = 0; i < argNums.length-1; i++) {
+    if (argNums[i] > argNums[i+1]) {
+      var temp = argNums[i]
+      argNums[i] = argNums[i+1]
+      argNums[i+1] = temp
+      swapped = true
+    }
+  }
+} while (swapped)
+
+console.log (argNums)
